cb4a205e98c0ff72d6ff1124c109dcbab4b56c25
Henrique Ribeiro Evangelista <henrique.rev@gmail.com>
2020-03-28 10:15:35 -0300

    listagem de incidents com dados da ong

--- src/controller/IncidentController.js
+++ src/controller/IncidentController.js
@@ -23,8 +23,12 @@ module.exports = {
     async index(request, response) {
         let { page = 1 } = request.query //Numero da pagina
 
-        //Com paginação
-        let incidents = await connection('incidents').limit(5).offset((page - 1) * 5).select('*')
+        //Com paginação e join
+        let incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.email', 'ongs.city', 'ongs.uf'])
 
         //Quantidade de incidents
         let [ count ] = await connection('incidents').count()
