22fab4c904e31cce42b530f4a94159526c48ebc0
Henrique Ribeiro Evangelista <henrique.rev@gmail.com>
2020-03-28 10:21:10 -0300

    configurando CORS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const routes = require('./routes')
 const app = express();
+const cors = require('cors')
+
+//Configurando CORS
+app.use(cors()) //permitir que todos os endereços acessem a aplicação
+
+/*
+Definindo rotas especificas na configuração do CORS
+
+    app.use(cors({
+        origin: 'http://urlDoFrontend.com'
+    }))
+*/
 
 //Informar para o express que os bodys virão no formato JSON
 app.use(express.json())
+
 app.use(routes)
 
 app.listen(3333);
